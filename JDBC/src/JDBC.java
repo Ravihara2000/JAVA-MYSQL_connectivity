6f74d9eb6543d898cca039ac810e229c03775e6b
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2023-08-14 09:25:21 +0530

    created the connection

--- JDBC/src/JDBC.java
+++ JDBC/src/JDBC.java
@@ -1,10 +1,16 @@
+import java.sql.*;
 public class JDBC {
     public static void main(String[] args) {
-        try{
-            class.forName()
-        }
+        String url = "jdbc:mysql://localhost:3386/jdbc";
+        String user = "root";
+        String password = "Ravihara13346";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection=DriverManager.getConnection(url,user,password);
 
-    }catch (Exception e){
+
+        } catch (Exception e) {
             System.out.println(e);
         }
+    }
 }
