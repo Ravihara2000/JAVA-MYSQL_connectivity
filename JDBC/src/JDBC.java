b12f1cf10ccc3a90a886c284bfbf2bc7852686bb
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2023-08-14 09:15:15 +0530

    connector dependencies added
    created project and downloaded MySql connector dependencies

---
+++ JDBC/src/JDBC.java
@@ -0,0 +1,10 @@
+public class JDBC {
+    public static void main(String[] args) {
+        try{
+            class.forName()
+        }
+
+    }catch (Exception e){
+            System.out.println(e);
+        }
+}
