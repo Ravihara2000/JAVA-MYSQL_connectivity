4411755701e30d61224eda91db2a47433bf0dee3
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2023-08-14 09:44:18 +0530

    create variables to show db data

--- JDBC/src/JDBC.java
+++ JDBC/src/JDBC.java
@@ -4,12 +4,22 @@ public class JDBC {
         String url = "jdbc:mysql://localhost:3386/jdbc";
         String user = "root";
         String password = "Ravihara13346";
-        String query="";
+        String query="SELECT * FROM users";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection=DriverManager.getConnection(url,user,password);
             Statement statement = connection.createStatement();
             statement.executeQuery(query);
+            ResultSet result = statement.executeQuery(query);
+
+           String name;
+            int age;
+            while(result.next()){
+                name = result.getString(1);
+                age= result.getInt(2);
+                System.out.println(name + " "+age);
+            }
+
 
         } catch (Exception e) {
             System.out.println(e);
