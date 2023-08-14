66fc4c1e120928a562c1f001affedf657bd1333f
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2023-08-14 09:29:12 +0530

    create statement to run query

--- JDBC/src/JDBC.java
+++ JDBC/src/JDBC.java
@@ -4,10 +4,12 @@ public class JDBC {
         String url = "jdbc:mysql://localhost:3386/jdbc";
         String user = "root";
         String password = "Ravihara13346";
+        String query="";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection=DriverManager.getConnection(url,user,password);
-
+            Statement statement = connection.createStatement();
+            statement.executeQuery(query);
 
         } catch (Exception e) {
             System.out.println(e);
