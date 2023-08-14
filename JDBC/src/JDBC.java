bdb86b89ef4313a24fb743bcef0f9b5d28ccfc50
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2023-08-14 10:01:36 +0530

    new data interted

--- JDBC/src/JDBC.java
+++ JDBC/src/JDBC.java
@@ -1,24 +1,26 @@
 import java.sql.*;
 public class JDBC {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3386/jdbc";
+        String url = "jdbc:mysql://localhost:3306/jdbc";
         String user = "root";
-        String password = "Ravihara13346";
-        String query="SELECT * FROM users";
+        String password = "www+ravi2000";
+        //String query="SELECT * FROM users";
+        String query="INSERT INTO users VALUES ('gayan',28)";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection=DriverManager.getConnection(url,user,password);
             Statement statement = connection.createStatement();
-            statement.executeQuery(query);
-            ResultSet result = statement.executeQuery(query);
+            statement.executeUpdate(query);
+            //statement.executeQuery(query);
+            //ResultSet result = statement.executeQuery(query);
 
-           String name;
+/*           String name;
             int age;
             while(result.next()){
                 name = result.getString(1);
                 age= result.getInt(2);
                 System.out.println(name + " "+age);
-            }
+            }*/
 
             connection.close();
 
